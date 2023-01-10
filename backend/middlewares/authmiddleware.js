edd195013361fd7330b9fe51e86553e0471d55e8
Sriharikrishna Agnihotram <93901685+srihari667@users.noreply.github.com>
2023-01-10 15:34:00 +0530

    Add files via upload

---
+++ backend/middlewares/authmiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const { userModel } = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1]
+    if (token) {
+      const result = await jwt.verify(token, process.env.JWT_SECRET)
+
+      const getUser = await userModel.findById(result.id).select('-password')
+      req.user = getUser
+      next()
+    } else {
+      res.status(400)
+      throw new Error('token not available')
+    }
+  }
+})
+
+module.exports = { protect }
---
+++ backend/middlewares/errorhandlers.js
@@ -0,0 +1,7 @@
+const ErrorHandlers = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500
+  res.status(statusCode)
+  res.json({ message: err.message })
+}
+
+module.exports = { ErrorHandlers }
